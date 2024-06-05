e3cec96442bab5f45228bfdd23d347fe27a94b31
RobertoAguirre <roberto.aguirre.rodriguez@gmail.com>
2024-06-04 20:54:49 -0600

    Primeros cambios

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import Usuarios from './Usuarios';
+import Pokemon from './Pokemon';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+      <Usuarios/>
+      <Pokemon/>
     </div>
   );
 }
